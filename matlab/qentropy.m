8dd335e9acbd190e51e6b171cade74668cac2181
Sean Young <seannz@gmail.com>
2019-06-28 04:57:50 -0700

    log the rate and dist history

--- matlab/qentropy.m
+++ matlab/qentropy.m
@@ -2,5 +2,5 @@ function bits = qentropy(symbols)
     [~,~,symbols] = unique(symbols(:));
     counts = accumarray(symbols(:),1);
     counts = counts/sum(counts);
-    bits = -length(symbols(:))*sum(counts.*log2(counts));
+    bits = max(0,-length(symbols(:))*sum(counts.*log2(counts)));
 end
--- matlab/qntz.m
+++ matlab/qntz.m
@@ -6,30 +6,43 @@ imds = imageDatastore('~/Developer/img','ReadFcn',@vgg16reader);
 neural = vgg16;
 layers = [neural.Layers(1:39);regressionLayer('Name','output')];
 gtruth = predict(assembleNetwork(layers),imds);
+normsq = sum(gtruth(:).^2);
 
 l = 2;
 [h,w,p,q] = size(layers(l).Weights);
 
-for i = 1:q
+hist_coded = NaN*zeros(16,q);
+hist_relse = NaN*zeros(16,q);
+
+
+for i = 1:1 % iterate over output channels
     quant = layers;
     convw = quant(l).Weights(:,:,:,i);
     biasw = quant(l).Bias(i);
-    scale = std(convw(:));
+    scale = 2^floor(log2(std(convw(:))/1024));
     coded = Inf;
-
-    delta = 2^floor(log2(scale/256));
-    while (coded > 0)
+    for j = 1:16
+        if coded == 0
+            break
+        end
+        delta = scale*2^(j-1);
         % quantize each of the q slices
-        convw = quantize(convw,delta); 
-        biasw = quantize(biasw,delta);
-        coded = qentropy([convw(:);biasw]);
+        convq = quantize(convw,delta);
+        biasq = quantize(biasw,delta);
         % assemble the network again
-        quant(l).Weights(:,:,:,i) = convw;
-        quant(l).Bias(i) = biasw;
+        quant(l).Weights(:,:,:,i) = convq;
+        quant(l).Bias(i) = biasq;
         net = assembleNetwork(quant);
         % run the prediction
-        mse = mean(sum((predict(net,imds) - gtruth).^2,2),1);
-        delta = 2 * delta;
+        preds = predict(net,imds);
+        relse = sum((preds(:) - gtruth(:)).^2)/normsq;
+        coded = qentropy([convq(:);biasq]);
+
+        hist_coded(j,i) = coded;
+        hist_relse(j,i) = relse;
+        
+        disp(sprintf('output %3d, delta: %5.2e, relsse: %5.2e, length: %5.2e',...
+                     i, delta, relse, coded));
     end
 end
     
