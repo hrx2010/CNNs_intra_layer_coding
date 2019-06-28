6d871c2f5a9d76937ddcfb92ea08fb9360dc04b9
Sean Young <seannz@gmail.com>
2019-06-28 13:21:23 -0700

    qentropy now measures entropy rather than coded length of all symbols, vgg16 can handl greyscale images now too

--- matlab/qntz.m
+++ matlab/qntz.m
@@ -1,48 +1,53 @@
 clear all;
 close all;
 
-imds = imageDatastore('~/Developer/img','ReadFcn',@vgg16reader);
+imds = imageDatastore('~/Developer/ILSVRC2012/ILSVRC2012_test_00000*.JPEG','ReadFcn',@vgg16reader);
 
 neural = vgg16;
 layers = [neural.Layers(1:39);regressionLayer('Name','output')];
-gtruth = predict(assembleNetwork(layers),imds);
-normsq = sum(gtruth(:).^2);
+neural = assembleNetwork(layers);
 
 l = 2;
 [h,w,p,q] = size(layers(l).Weights);
 
-hist_coded = NaN*zeros(16,q);
-hist_relse = NaN*zeros(16,q);
+hist_coded = zeros(16,q)*NaN;
+hist_relse = zeros(16,q);
+norm2 = 0;
 
-
-for i = 1:1 % iterate over output channels
-    quant = layers;
-    convw = quant(l).Weights(:,:,:,i);
-    biasw = quant(l).Bias(i);
-    scale = 2^floor(log2(std(convw(:))/1024));
-    coded = Inf;
-    for j = 1:16
-        if coded == 0
-            break
+for f = 1:length(imds.Files)
+    X = imds.readimage(f);
+    Y = predict(neural,X);
+    Y_ssq = sum(Y(:).^2);
+    norm2 = norm2 + Y_ssq;
+    for i = 1:1 % iterate over output channels
+        quant = layers;
+        convw = quant(l).Weights(:,:,:,i);
+        biasw = quant(l).Bias(i);
+        scale = 2^floor(log2(std(convw(:))/1024));
+        coded = Inf;
+        for j = 1:16
+            if coded == 0
+                break
+            end
+            delta = scale*2^(j-1);
+            % quantize each of the q slices
+            convq = quantize(convw,delta);
+            biasq = quantize(biasw,delta);
+            % assemble the network again
+            quant(l).Weights(:,:,:,i) = convq;
+            quant(l).Bias(i) = biasq;
+            net = assembleNetwork(quant);
+            % run the prediction
+            Y_hat = predict(net,X);
+            error = sum((Y_hat(:) - Y(:)).^2);
+            coded = qentropy([convq(:);biasq]);
+            
+            hist_coded(j,i) = coded;
+            hist_relse(j,i) = hist_relse(j,i) + error;
+            
+            [~,filename] = fileparts(imds.Files{f});
+            disp(sprintf('%s, output %3d, delta: %5.2e, relsse: %5.2e, rate: %5.2e',...
+                         filename, i, delta, sqrt(error/Y_ssq), coded));
         end
-        delta = scale*2^(j-1);
-        % quantize each of the q slices
-        convq = quantize(convw,delta);
-        biasq = quantize(biasw,delta);
-        % assemble the network again
-        quant(l).Weights(:,:,:,i) = convq;
-        quant(l).Bias(i) = biasq;
-        net = assembleNetwork(quant);
-        % run the prediction
-        preds = predict(net,imds);
-        relse = sum((preds(:) - gtruth(:)).^2)/normsq;
-        coded = qentropy([convq(:);biasq]);
-
-        hist_coded(j,i) = coded;
-        hist_relse(j,i) = relse;
-        
-        disp(sprintf('output %3d, delta: %5.2e, relsse: %5.2e, length: %5.2e',...
-                     i, delta, relse, coded));
     end
-end
-    
+end    
--- matlab/vgg16reader.m
+++ matlab/vgg16reader.m
@@ -1,3 +1,6 @@
 function img = vgg16reader(filename)
     img = imresize(imread(filename),[224,224]);
+    if size(img,3) == 1
+        img = repmat(img,[1,1,3]);
+    end
 end
