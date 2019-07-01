f3f9f4df3be607cfb4d1575ceb48345924acd77a
Sean Young <seannz@gmail.com>
2019-06-30 22:19:32 -0700

    plotrd script with huber fitting function

---
+++ matlab/plotrd.m
@@ -0,0 +1,24 @@
+clear all;
+close all;
+
+archname = 'alexnet';
+load(archname);
+
+figure(1);
+plot(hist_coded(:,:,1),-10*log10(squeeze(mean(hist_Y_sse,3))));
+xticks(0:2:8);
+yticks(-50:50:100)
+xlabel('rate (bits)');
+ylabel('Noise Power (dB)');
+axis([0,8,-50,100]);
+%pdfprint('temp.pdf','Width',20,'Height',12,'Position',[3.5,3,15.5,8]);
+
+figure(2);
+line_Y_sse = huberfit(hist_coded(:,:,1),mean(hist_Y_sse,3));
+plot(hist_coded(:,:,1),line_Y_sse);
+xticks(0:2:8);
+yticks(-50:50:100)
+xlabel('rate (bits)');
+ylabel('Regres. Noise Power (dB)');
+axis([0,8,-50,100]);
+%pdfprint('temp2.pdf','Width',20,'Height',12,'Position',[3.5,3,15.5,8]);
