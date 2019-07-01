e861c8ac08b3e0128b70ceacde40bf1e7b6fe96d
Sean Young <seannz@gmail.com>
2019-06-30 23:21:00 -0700

    move the - to out of the huber function

--- matlab/huberfit.m
+++ matlab/huberfit.m
@@ -5,7 +5,7 @@ function Y_hat = huberfit(X,Y,slope)
     if nargin == 2
         % get the slope (should be around 6.01)
         x = X(~isnan(X));
-        y = -10*log10(Y(~isnan(Y)));
+        y = 10*log10(Y(~isnan(Y)));
         A = [x(:),ones(length(x(:)),1)];
         p = A\y;
         slope = p(1);
@@ -13,7 +13,7 @@ function Y_hat = huberfit(X,Y,slope)
 
     Y_hat = zeros(m,n);
     for j = 1:n
-        sigma = mean(-10*log10(Y(:,j)) - slope*X(:,j),'omitnan');
+        sigma = mean(10*log10(Y(:,j)) - slope*X(:,j),'omitnan');
         Y_hat(:,j) = slope*X(:,j) + sigma;
     end
 end
