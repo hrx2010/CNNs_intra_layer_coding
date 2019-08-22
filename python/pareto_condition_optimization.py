caaf9b4b1c33fc06a503abf971608380e0c286b9
hrx2010 <mark.wangzhe@gmail.com>
2019-08-22 20:09:19 +0800

    add layer-wise optimization function

--- python/pareto_condition_optimization.py
+++ python/pareto_condition_optimization.py
@@ -51,6 +51,54 @@ def pareto_condition_optimization(num_conv_layers , hist_filter_dims , total_rat
 		else:
 			right_bound = mid
 
+
+def pareto_condition_optimization_layer_wise(num_conv_layers , hist_num_weights , total_rate , num_quant_levels , hist_coded , hist_delta):
+	left_bound = -1e12
+	right_bound = 0.0
+
+	solutions = [0] * num_conv_layers
+	bit_allocations = [0] * num_conv_layers
+
+	count_itr = 0
+
+	while (right_bound - left_bound > eps_slope):
+		mid = (left_bound + right_bound) / 2
+		cur_slope = mid
+		count_itr = count_itr + 1
+
+		#print('left %f right %f: mid %f' % (left_bound , right_bound , mid))
+
+		total_compressed_size = 0.0
+		total_output_error = 0.0
+
+		for i in range(num_conv_layers):
+			#bit_allocations[i] = np.zeros((hist_filter_dims[i]))
+			#for j in range(hist_filter_dims[i]):
+
+				min_intercept = INF
+				allocated_bits = -1
+
+				for k in range(num_quant_levels):
+					cur_R = hist_coded[i][k]
+					cur_D = hist_delta[i][k]
+					cur_intercept = cur_D - cur_slope * cur_R
+
+					if (cur_intercept < min_intercept or k == 0):
+						min_intercept = cur_intercept
+						allocated_bits = k
+
+				bit_allocations[i] = allocated_bits
+				total_compressed_size += hist_coded[i][allocated_bits]
+
+		if total_compressed_size < total_rate:
+			for i in range(num_conv_layers):
+				solutions[i] = bit_allocations[i]
+
+		if total_compressed_size < total_rate:
+			left_bound = mid
+		else:
+			right_bound = mid
+
 	return solutions
 
 				
