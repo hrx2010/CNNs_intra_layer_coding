1e69b1e8bc58d4de1b3676ad99af80f597bf3752
Sean Young <seannz@gmail.com>
2019-08-26 19:06:45 -0700

    comp_stats and plotrdfreq updates for paper figures

--- matlab/comp_stats.m
+++ matlab/comp_stats.m
@@ -10,51 +10,57 @@ imagedir = '~/Developer/ILSVRC2012_val/*.JPEG';
 labeldir = './ILSVRC2012_val.txt';
 tranname = 'dft2';
 testsize = 1024;
-maxsteps = 64;
 
 [neural,images] = loadnetwork(archname,imagedir, labeldir, testsize);
 [layers,lclass] = removeLastLayer(neural);
 neural = assembleNetwork(layers);
-nclass = assembleNetwork(lclass);
-trans = {str2func(tranname), str2func(['i',tranname])};
 
 l_kernel = findconv(neural.Layers);
 l_length = length(l_kernel);
 
-hist_delta = cell(l_length,1);
-hist_coded = cell(l_length,1);
-hist_W_sse = cell(l_length,1);
-hist_Y_sse = cell(l_length,1);
-hist_Y_top = cell(l_length,1);
-
-hist_gainX = cell(l_length,1);
-hist_gainT = cell(l_length,1);
-hist_coded = ones(l_length,1);
+klt2 = @(x) gklt(x,adf);
+trannames = {'dst2','fst2'};
+t_length = length(trannames) + 1;
+hist_gainX = cell(l_length,t_length);
+hist_gainT = cell(l_length,t_length);
+hist_coded = ones(l_length,t_length);
 
 layers = neural.Layers(l_kernel);
+
 for l = 1:l_length
-    X = activations(neural,images,neural.Layers(l_kernel(l)-1).Name);
-    [H,W,P,Q] = size(X);
-    [h,w,p,q,g] = size(layers(l).Weights);
-    T = reshape(permute(layers(l).Weights,[1,2,3,5,4]),[h,w,p*g,q]);
+        X = activations(neural,images,neural.Layers(l_kernel(l)-1).Name);
+        [H,W,P,Q] = size(X);
+        [h,w,p,q,g] = size(layers(l).Weights);
+        T = reshape(permute(layers(l).Weights,[1,2,3,5,4]),[h,w,p*g,q]);
+        T_ssd = mean(mean(T.^2,1),2);
 
-    X_psd = real(diag3(conj(permute(blkfft2(conj(permute(blkfft2(blktoeplitz(autocorr2(X,h,w)),h,w),[2,1,3])),h,w),[2,1,3]))));
-    T_psd = mean(abs(fft2(T)).^2,4)*(1/h/w);
-    T_ssd = mean(mean(T.^2,1),2);
+        for i = 1:length(trannames)
+            tranname = trannames{i};
+            trans = str2func(tranname);
+            X_psd = diag3(permute(blkfft2(permute(blkfft2(blktoeplitz(autocorr2(X,h,w)),h,w,trans),[2,1,3]),h,w,trans),[2,1,3]));
+            T_psd = mean(abs(trans(T)).^2,4)*(1/h/w);
 
-    hist_gainX{l} = mean(X_psd(:))/geomean(X_psd(:));
-    %hist_gainX{l} = geomean(X_ssd(:))/geomean(X_psd(:));
-    hist_gainT{l} = geomean(T_ssd(:))/geomean(T_psd(:));
-    hist_coded(l) = h*w*p*q*g;
+            X_psd(find(X_psd<0)) = NaN;
+            hist_meanX{l,i} = mean(X_psd(:));
+            hist_geomX{l,i} = geomean(X_psd(:));
 
-    disp(sprintf('Coding gain for layer %03d is %5.2f (%5.2f, %5.2f) dB (%5d coefficients)', ...
-                 l, 10*log10(mean(hist_gainX{l}.*hist_gainT{l})), 10*log10(mean(hist_gainX{l})), ...
-                 10*log10(mean(hist_gainT{l})), hist_coded(l)));
+            hist_meanT{l,i} = mean(T_psd(:));
+            hist_geomT{l,i} = geomean(T_psd(:));
+            hist_coded(l,i) = h*w*p*q*g;
+
+            gain_X = hist_meanX{l,i}/hist_geomX{l,i};
+            gain_T = hist_meanT{l,i}/hist_geomT{l,i};
+            disp(sprintf('Coding gain for layer %03d is %5.2f (%5.2f, %5.2f) dB (%5d coefficients)', ...
+                         l, 10*log10(gain_X*gain_T), 10*log10(gain_X), 10*log10(gain_T), hist_coded(l,i)));
+        end
+
+        % hist_gainT{l,end} = mean(T_ssd(:))/geomean(T_ssd(:));
+
+
+        % mean_gainX = sum(cell2mat(hist_gainX{:,i}).*hist_coded{:,i})/sum(hist_coded{:,i});
+        % mean_gainT = sum(cell2mat(hist_gainT{:,i}).*hist_coded{:,i})/sum(hist_coded{:,i});
+        % mean_gains = sum(cell2mat(hist_gainX{:,i}).*cell2mat(hist_gainT{:,i}).*hist_coded{:,i})/sum(hist_coded{:,i});
+
+        % disp(sprintf('Coding gain for layer all is %5.2f (%5.2f, %5.2f) dB (%5d coefficients)', ...
+        %              10*log10(mean_gains), 10*log10(mean_gainX), 10*log10(mean_gainT), sum(hist_coded{:,i})));
 end
-
-mean_gainX = sum(cell2mat(hist_gainX).*hist_coded)/sum(hist_coded);
-mean_gainT = sum(cell2mat(hist_gainT).*hist_coded)/sum(hist_coded);
-mean_gains = sum(cell2mat(hist_gainX).*cell2mat(hist_gainT).*hist_coded)/sum(hist_coded);
-
-disp(sprintf('Coding gain for layer all is %5.2f (%5.2f, %5.2f) dB (%5d coefficients)', ...
-             10*log10(mean_gains), 10*log10(mean_gainX), 10*log10(mean_gainT), sum(hist_coded)));
--- matlab/plotrdfreq.m
+++ matlab/plotrdfreq.m
@@ -3,14 +3,17 @@ close all;
 
 archname = 'alexnet';
 testsize = 1024;
-trans = {'idt2','dft2','idt2_base'};
+outlayer = 'output';
+inlayers = 1:5;
+trans = {'idt2','idt2_base','dct2','dst2'};
+
 X = cell(length(trans),1);
 Y = cell(length(trans),1);
 Z = cell(length(trans),1);
 
 for i = 1:length(trans)
     tranname = trans{i};
-    load(sprintf('%s_%s_sum_%d',archname,tranname,testsize));
+    load(sprintf('%s_%s_sum_%d_%d_%d_%s',archname,tranname,testsize,inlayers(1),inlayers(end),outlayer));
     X{i} = mean(hist_sum_coded,3);
     Y{i} = mean(hist_sum_Y_sse,3);
     Z{i} = mean(hist_sum_Y_top,3);
@@ -26,15 +29,15 @@ yticks(10.^(-4:1:2));
 axis([0,8,10^-4,10^2]);
 xlabel('Rate (bits)');
 ylabel('Mean squared error');
-%pdfprint('temp2.pdf','Width',21,'Height',12,'Position',[3.5,3,16.5,8]);
+pdfprint('temp2.pdf','Width',14,'Height',12,'Position',[3,2.5,10,8.5]);
 
 figure(2);
-plot(X,100*Z,'.-','MarkerSize',8);
-hold on;
-plot(X,100*Z(1,1)*ones(size(X,1),1),'-','Color',0.5*[1,1,1]);
+plot(X,100*Z/Z(1,1),'.-','MarkerSize',8);
+% hold on;
+% plot(X,100*Z(1,1)*ones(size(X,1),1),'-','Color',0.5*[1,1,1]);
 xticks(0:8);
-yticks(0:10:60);
-axis([0,8,0,60]);
+yticks(0:20:100);
+axis([0,8,0,100]);
 xlabel('Rate (bits)');
-ylabel('Top-1 accuracy (\%)');
-%pdfprint('temp3.pdf','Width',21,'Height',12,'Position',[3.5,3,16.5,8]);
+ylabel('Relative accuracy (\%)');
+pdfprint('temp3.pdf','Width',14,'Height',12,'Position',[3,2.5,10,8.5]);
