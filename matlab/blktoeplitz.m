ada817a73b65b122c41e8aee753a1073e64b4e57
Sean Young <seannz@gmail.com>
2019-09-02 05:28:44 -0700

    remove the 3rd dimension in the blk toeplitz matrix

--- matlab/blktoeplitz.m
+++ matlab/blktoeplitz.m
@@ -9,12 +9,12 @@ function T = blktoeplitz(c)
 %      integer: uint8, int8, uint16, int16, uint32, int32, uint64, int64
 %
 
-[p,q,r] = size(c);
-x = [c(p:-1:2,:,:);c];                  % build vector of user data
-i = (0:p-1)' + (p:-1:1);                % Toeplitz subscripts
-t = reshape(x(i,:,:),p,p,[]);           % actual data
+[p,q] = size(c);
+x = [c(p:-1:2,:);c];                  % build vector of user data
+i = (0:p-1)' + (p:-1:1);              % Toeplitz subscripts
+t = reshape(x(i,:),p,p,q);           % actual data
 
-T = reshape(mat2cell(reshape(t,p,[]),p,p*ones(1,q*r)),[1,q,r]);
-X = [T(:,q:-1:2,:),T];                  % build vector of user data
+T = mat2cell(reshape(t,p,p*q),p,p*ones(1,q));
+X = [T(:,q:-1:2),T];                  % build vector of user data
 j = (0:q-1)' + (q:-1:1);                % Toeplitz subscripts
-T = cell2mat(reshape(X(:,j,:),q,q,[])); % actual data
+T = cell2mat(reshape(X(:,j),q,q)); % actual data
