5c0dabfc69755734db98d4be1cf68e4bf06ac3b0
Sean Young <seannz@gmail.com>
2019-10-05 21:44:31 -0700

    generate KL basis

--- matlab/generate_KL_basis.m
+++ matlab/generate_KL_basis.m
@@ -1,4 +1,8 @@
-function K = generate_KL_basis(archname,testsize)
+function K = generate_KL_basis(archname,testsize,klttype)
+    if nargin < 3
+        klttype = 'kklt';
+    end
+
     imagedir = '~/Developer/ILSVRC2012_val/*.JPEG';
     labeldir = './ILSVRC2012_val.txt';
 
@@ -16,14 +20,21 @@ function K = generate_KL_basis(archname,testsize)
     for l = 1:l_length
         layer = layers(l);
         [h,w,p,q] = size(layer.Weights);
-        X = getx(neural,nclass,images,layer.Name);
-        covX = blktoeplitz(autocorr3(X,h,w));
-        invcovX = inv(covX);
+        switch klttype
+          case 'kklt'
+            X = activations(neural,images,neural.Layers(l_kernel(l)-1).Name);
+            X = X - mean(mean(mean(X,1),2),4); % subtract per-channel means % X = getx(neural,nclass,images,layer.Name);
+            covX = blktoeplitz(autocorr3(X,h,w));
+          case 'klt'
+            covX = eye(h*w);
+        end
+
+        invcovX = inv(covX+covX');
         covH = cov(reshape(layer.Weights,[h*w,p*q])');
-        [V,~] = eig(covH,(invcovX+invcovX')/2);
-        K{l,2} = V;
-        K{l,1} = inv(V);
-        disp(sprintf('%s klt2 | generated transform for layer %03d', archname, l));
+        [V,~] = eig(covH+covH',invcovX+invcovX');
+        K{l,2} = V';
+        K{l,1} = inv(V');
+        disp(sprintf('%s %s | generated transform for layer %03d', archname, klttype, l));
     end
-    save(sprintf('%s_%s',archname,'klt2'),'K');
+    save(sprintf('%s_%s',archname,klttype),'K');
 end
