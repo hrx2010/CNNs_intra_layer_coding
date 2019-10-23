0b7a84e35f4c5c872b69814101eba14f56a8ca74
Sean Young <seannz@gmail.com>
2019-10-23 17:10:50 +1100

    alexnet does not use crop preprocessing of images

--- matlab/read227x227.m
+++ matlab/read227x227.m
@@ -1,10 +1,11 @@
 function img = read227x227(filename)
     img = imread(filename);
+    img = imresize(img,[227,227]);
     [h,w,d] = size(img);
-    newdim = floor((256/min(h,w))*[h,w]);
-    offset = floor((newdim - 227)/2);
-    img = imresize(img,newdim,'bilinear');
-    img = img(offset(1)+(0:226),offset(2)+(0:226),:);
+    % newdim = floor((256/min(h,w))*[h,w]);
+    % offset = floor((newdim - 227)/2);
+    % img = imresize(img,newdim,'bilinear');
+    % img = img(offset(1)+(0:226),offset(2)+(0:226),:);
     if d == 1
         img = repmat(img,[1,1,3]);
     end
