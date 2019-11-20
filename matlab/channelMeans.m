73fe1052d1e6b46cb1fc4ed8c636655c0c236516
Sean Young <seannz@gmail.com>
2019-11-20 10:30:50 -0800

    subtract the means, then add offset to biases

--- matlab/channelMeans.m
+++ matlab/channelMeans.m
@@ -1,4 +1,4 @@
-function means = channelMeans(neural,images)
+function [means,offset] = channelMeans(neural,images)
 %GENERATE_KL_INTER Generate KL transform for inter-kernel coding.
 %   K = GENERATE_KL_INTRA(ARCHNAME,TESTSIZE,KLTTYPE,DIMTYPE)
 %   generates the Karhunen-Loeve transform K for neural network
@@ -20,6 +20,7 @@ function means = channelMeans(neural,images)
     l_length = length(l_kernel);
 
     means = cell(l_length,1);
+    offset = cell(l_length,1);
     layers = neural.Layers(l_kernel);
 
     for l = 1:l_length
@@ -27,5 +28,9 @@ function means = channelMeans(neural,images)
         [h,w,p,q,g] = size(layer.Weights);
         X = activations(neural,images,neural.Layers(l_kernel(l)-1).Name);
         means{l} = mean(mean(mean(X,1),2),4); % subtract per-channel means % X = getx(neural,nclass,images,layer.Name);
+        offset{l} = zeros(1,1,q,g);
+        for k = 1:g
+            offset{l}(1,1,:,k) = squeeze(sum(means{l}(:,:,(k-1)*p+(1:p)).*sum(sum(layer.Weights(:,:,:,:,k),1),2)));
+        end
     end
 end
