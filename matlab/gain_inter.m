92f114514d7226f72129c3fa0bfe1bf7d88cce99
Sean Young <seannz@gmail.com>
2020-01-21 00:04:21 -0800

    loadnewtork

--- matlab/gain_inter.m
+++ matlab/gain_inter.m
@@ -1,7 +1,7 @@
 clear all;
 close all;
 
-archname = 'alexnet';
+archname = 'vgg16';
 imagedir = '~/Developer/ILSVRC2012_val/*.JPEG';
 labeldir = './ILSVRC2012_val.txt';
 testsize = 100;
@@ -13,26 +13,31 @@ neural = assembleNetwork(layers);
 l_kernel = findconv(neural.Layers);
 l_length = length(l_kernel);
 
-trannames = {'idt_5000_inter','klt_5000_inter','kkt_5000_inter'};
+trannames = {'idt_100_inter','klt_100_inter','kkt_100_inter'};
 t_length = length(trannames);
 
 gains = zeros(l_length,t_length,2);
 layers = neural.Layers(l_kernel);
-for l = 1:5%l_length
+varH = cell(l_length,length(trannames));
+varX = cell(l_length,length(trannames));
+
+for l = [1:2:13,14:16]%l_length
      layer = layers(l);
-     [h,w,p,q,g] = size(layer.Weights);
+     [h,w,p,q,g] = size(perm5(layer.Weights,layer));
      X = activations(neural,images,neural.Layers(l_kernel(l)-1).Name);
      X = X - mean(mean(mean(X,1),2),4); % subtract per-channel means
      H = layer.Weights;
 
      for t = 1:length(trannames)
          T = gettrans(trannames{t},archname,l);
-         varH = mean(reshape(permute(transform_inter(H,T{1}),[3,5,1,2,4]),p,g,[]).^2,3);
-         varX = mean(reshape(permute(transform_inter(X,T{3}),[3,5,1,2,4]),p,g,[]).^2,3);
-         gains(l,t,1) = geomean(varX(:));
-         gains(l,t,2) = geomean(varH(:));
+         varH{l,t} = mean(reshape(permute(reshape5(transform_inter(perm5(H,layer),T{1}),T{1}),[3,5,1,2,4]),p,g,[]).^2,3);
+         varX{l,t} = mean(reshape(permute(reshape5(transform_inter(X,T{3}),T{3}),[3,5,1,2,4]),p,g,[]).^2,3);
+         gains(l,t,1) = geomean(varX{l,t}(:));
+         gains(l,t,2) = geomean(varH{l,t}(:));
          disp(sprintf('%s %s | layer %03d (%5d coefficients) is %5.2f %5.2f %5.2f dB ', ...
                       archname, trannames{t}, l, numel(H), 10*log10(gains(l,t,1)), ...
                       10*log10(gains(l,t,2)), sum(10*log10(gains(l,t,:)))));
      end
 end
+
+save(sprintf('%s_gain_inter.mat',archname),'varH','varX')
--- matlab/gain_intra.m
+++ matlab/gain_intra.m
@@ -1,7 +1,7 @@
 clear all;
 close all;
 
-archname = 'alexnet';
+archname = 'vgg16';
 imagedir = '~/Developer/ILSVRC2012_val/*.JPEG';
 labeldir = './ILSVRC2012_val.txt';
 testsize = 100;
@@ -13,12 +13,15 @@ neural = assembleNetwork(layers);
 l_kernel = findconv(neural.Layers);
 l_length = length(l_kernel);
 
-trannames = {'idt','dct2_2','dst','klt_5000_intra','kkt_5000_intra'};
+trannames = {'idt','dct','dct2_2','klt_100_intra','kkt_100_intra'};
 t_length = length(trannames);
 
 gains = zeros(l_length,t_length,2);
 layers = neural.Layers(l_kernel);
-for l = 1:5%l_length
+varH = cell(l_length,length(trannames));
+varX = cell(l_length,length(trannames));
+
+for l = 1:2:13%l_length
      layer = layers(l);
      [h,w,p,q,g] = size(layer.Weights);
      X = activations(neural,images,neural.Layers(l_kernel(l)-1).Name);
@@ -27,12 +30,14 @@ for l = 1:5%l_length
 
      for t = 1:length(trannames)
          T = gettrans(trannames{t},archname,l);
-         varH = hvars2(H,T{1},h,w);
-         varX = xvars2(X,T{3},h,w);
-         gains(l,t,1) = geomean(varX(:),'omitnan');
-         gains(l,t,2) = geomean(varH(:),'omitnan');
+         varH{l,t} = hvars2(H,T{1},h,w);
+         varX{l,t} = xvars2(X,T{3},h,w);
+         gains(l,t,1) = geomean(varX{l,t}(:),'omitnan');
+         gains(l,t,2) = geomean(varH{l,t}(:),'omitnan');
          disp(sprintf('%s %14s | layer %03d (%5d coefficients) is %5.2f %5.2f %5.2f dB ', ...
                       archname, trannames{t}, l, numel(H), 10*log10(gains(l,t,1)), ...
                       10*log10(gains(l,t,2)), sum(10*log10(gains(l,t,:)))));
      end
 end
+
+save(sprintf('%s_gain_intra.mat',archname),'varH','varX')
