6f3f0417013f27ae40e4b021d3d94c9e6ad071a0
Sean Young <seannz@gmail.com>
2020-02-05 03:58:31 -0800

    inter kern

--- matlab/generate_RD_frontier_inter_kern.m
+++ matlab/generate_RD_frontier_inter_kern.m
@@ -33,7 +33,7 @@ hist_sum_non0s = zeros(maxsteps,l_length)*NaN;
 hist_sum_total = zeros(maxsteps,l_length)*NaN;
 
 for j = 1:maxsteps
-    slope = -48 + 0.50*(j-1);
+    slope = -32 + 0.50*(j-1);
     ydist_kern = cell(l_length,1);
     coded_kern = cell(l_length,1);
     delta_kern = cell(l_length,1);
@@ -45,9 +45,9 @@ for j = 1:maxsteps
     for l = inlayers
         load(sprintf('%s_%s_val_1000_%d_%d_%s_inter_kern',archname,tranname,l,l,outlayer));
         basis_vectors = gettrans([tranname,'_50000_inter'],archname,l);
-        [h,w,p,q,g] = size(perm5(quants(l).Weights,quants(l),size(basis_vectors,1)));
-        quant_weights = reshape(permute(transform_inter(perm5(quants(l).Weights,quants(l),size(basis_vectors,1)),...
-                                                        basis_vectors(:,:,:,1)),[1,2,3,5,4]),[h,w,p*g,q]);
+        [h,w,p,q,g] = size(quants(l).Weights);
+        quant_weights = quants(l).Weights;
+        quant_weights = reshape(basis_vectors(:,:,:,1)*reshape(permute(quant_weights,[3,5,4,1,2]),p*g,q*h*w),p*g,q*h*w);
         [kern_best_Y_sse,kern_best_delta,kern_best_coded] = finddelta(mean(kern_Y_sse{l},4),kern_delta{l},kern_coded{l});
         ydist_kern{l} = lambda2points(kern_best_coded,kern_best_Y_sse,kern_best_Y_sse,2^slope);
         coded_kern{l} = lambda2points(kern_best_coded,kern_best_Y_sse,kern_best_coded,2^slope);
@@ -57,12 +57,15 @@ for j = 1:maxsteps
         s = strides(l);
         for i = 1:s:p*g%min(h*w*q,p*g)
             rs = i:min(p*q,s+i-1);
+            scale = floor(log2(sqrt(mean(reshape(quant_weights(rs,:,:,:),[],1).^2))));
+            if scale < -24 %all zeros
+                continue
+            end
             % quantize for the given lambda
-            quant_weights(:,:,rs,:) = quantize(quant_weights(:,:,rs,:),2^delta_kern{l}(i),coded_kern{l}(i)/(s*h*w*q));
+            quant_weights(rs,:,:,:) = quantize(quant_weights(rs,:,:,:),2^delta_kern{l}(i),coded_kern{l}(i)/(length(rs)*h*w*q));
 
         end
-        quants(l).Weights = perm5(transform_inter(permute(reshape(quant_weights,[h,w,p,g,q]),[1,2,3,5,4]),...
-                                                  basis_vectors(:,:,:,2)),quants(l),size(basis_vectors,1));
+        quants(l).Weights = permute(reshape(basis_vectors(:,:,:,2)*quant_weights,[p,g,q,h,w]),[4,5,1,3,2]);
         wdist_kern{l} = double(sum((quants(l).Weights(:) - neural.Layers(l_kernel(l)).Weights(:)).^2));
         non0s_kern{l} = sum(squeeze(max(max(max(abs(quant_weights),[],4),[],1),[],2))>1e-7);
         coded_kern{l} = sum(coded_kern{l},'omitnan');
@@ -91,11 +94,12 @@ for j = 1:maxsteps
     disp(sprintf('%s %s | slope: %+5.1f, ymse: %5.2e (%5.2e), wmse: %5.2e, top1: %4.1f, rate: %5.2e',...
                  archname, tranname, slope, hist_sum_Y_sse(j,1), pred_sum_Y_sse(j,1), ...
                  hist_sum_W_sse(j,1), 100*hist_sum_Y_top(j,1), hist_sum_coded(j,1)));
-    if hist_sum_coded(j) == 0
+    if hist_sum_coded(j) == 0 || ...
+       hist_sum_Y_top(j) <= 0.002
         break;
     end
 end
 
 save(sprintf('%s_%s_sum_%d_%d_%d_%s_inter_total',archname,tranname,testsize,inlayers(1),inlayers(end),outlayer),...
-     'hist_sum_coded','hist_sum_Y_sse','pred_sum_Y_sse','hist_sum_W_sse','hist_sum_Y_top','hist_sum_non0s',...
+     '-v7.3','hist_sum_coded','hist_sum_Y_sse','pred_sum_Y_sse','hist_sum_W_sse','hist_sum_Y_top','hist_sum_non0s',...
      'hist_sum_total','hist_sum_denom');
