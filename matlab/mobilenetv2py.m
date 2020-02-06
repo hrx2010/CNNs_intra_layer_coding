491896f172cd8bed46c43a241e02da7f306a5645
Sean Young <seannz@gmail.com>
2020-02-05 20:45:28 -0800

    mobilenetv2

--- matlab/mobilenetv2py.m
+++ matlab/mobilenetv2py.m
@@ -1,5 +1,19 @@
 function net = mobilenetv2py
-    load('mobilenetv2.mat','weight','biases','bnorm_mean','bnorm_vars');
+    %load('mobilenetv2.mat','weight','biases','bnorm_mean','bnorm_vars');
     net = mobilenetv2;
-    net = modifyConvParams(net,weight,biases,bnorm_mean,bnorm_vars);
+
+    switch class(net)
+      case 'SeriesNetwork'
+        lgraph = layerGraph(net.Layers);
+      case 'DAGNetwork'
+        lgraph = layerGraph(net);
+    end
+
+    %net = modifyConvParams(net,weight,biases,bnorm_mean,bnorm_vars);
+    oldlayer = lgraph.Layers(153);
+    newlayer = convolution2dLayer([1,1],1000,'Name','Logits','Weights',permute(oldlayer.Weights,[3,4,2,1]),...
+                                  'Bias',permute(oldlayer.Bias,[2,3,1]));
+    lgraph = replaceLayer(lgraph,oldlayer.Name,newlayer);
+    net = assembleNetwork(lgraph);
 end
+
