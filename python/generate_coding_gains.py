12190070e534d13f81e2379cec2008417aa93eee
Sean Young <seannz@gmail.com>
2020-03-01 16:33:21 -0800

    coding gain script

--- python/generate_coding_gains.py
+++ python/generate_coding_gains.py
@@ -21,6 +21,9 @@ perm, flip = getperm(trantype)
 
 gainw = [None] * len(layers)
 gaing = [None] * len(layers)
+areaw = [None] * len(layers)
+areag = [None] * len(layers)
+
 for l in range(0,len(layers)):
     G = torch.tensor(loadvarstats(archname,trantype,testsize)[0,l]).to(common.device);
     G = G/G.max()
@@ -35,8 +38,11 @@ for l in range(0,len(layers)):
 
     gainw[l] = (10*(Dwidt.log().mean().exp()/Dwklt.log().mean().exp()).log10().detach().cpu()).numpy()
     gaing[l] = (10*(Dgidt.log().mean().exp()/Dgklt.log().mean().exp()).log10().detach().cpu()).numpy()
+    areaw[l] = (10*(Dwidt.log10()-Dwklt.log10()).sort(descending=True)[0]).detach().cpu().numpy()
+    areag[l] = (10*(Dgidt.log10()-Dgklt.log10()).sort(descending=True)[0]).detach().cpu().numpy()
 
     print('%s %s | layer: %03d/%03d, coding gain: %5.2f %5.2f (%5.2f) dB' %\
           (archname, tranname, l, len(layers), gainw[l], gaing[l], gainw[l] + gaing[l]))
 
-io.savemat(('%s_%s_%s_gain_%04d.mat' % (archname,trantype,tranname,testsize)),{'gainw':gainw,'gaing':gaing})
+io.savemat(('%s_%s_%s_gain_%04d.mat' % (archname,trantype,tranname,testsize)),\
+           {'gainw':gainw,'gaing':gaing,'areaw':areaw,'areag':areag})
