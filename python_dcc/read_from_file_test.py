db7c2bbdadfb6a1f7c287b5f61e54e33a3922bfc
Sean I Young <siy0@kale.nmr.mgh.harvard.edu>
2021-01-23 15:36:34 -0500

    remove legacy code

--- python_dcc/read_from_file_test.py
+++
@@ -1,8 +0,0 @@
-file_in = open("RD_curves_layer_0" , "r")
-
-lines = file_in.readlines()
-
-for l in lines:
-	print(l)
-
-
--- python_dcc/scripts/run_alexnet_all_kkl1.sh
+++
@@ -1,8 +0,0 @@
-id_gpu=2
-data_path=/home/wangzhe/Documents/exp/exp_2019_10/10_26_DCC_KKLT_Alex_VGG/transform_matrices/alexnet_kkt1.mat
-transform_method=kkt1
-
-for i in {1..80..1}
-do
-	python alexnet_accuracy_imagenet_given_matrix.py $id_gpu $i $transform_method $data_path
-done
